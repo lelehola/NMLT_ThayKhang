bc2e34d44cc799fa10021fcb983e48a8eff0bc0a
anh tuan <102863743+lelehola@users.noreply.github.com>
2022-05-12 15:35:55 +0700

    Add files via upload

---
+++ Bai030/Bai030.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string getInputFileName(int);
+string getOutputFileName(int);
+void Nhap(float[][500], int&, int&, string);
+void Xuat(float[][500], int, int, string);
+void Xuat(float[][500], int, int);
+float TongDuong(float[][500], int, int);
+
+int main()
+{
+	float b[500][500];
+	int k;
+	int l;
+	for (int i = 1; i <= 1; i++)
+	{
+		string FileName = getInputFileName(i);
+		Nhap(b, k, l, FileName);
+
+		cout << "\n" << FileName << endl;
+		Xuat(b, k, l);
+
+		float kq = TongDuong(b, k, l);
+		cout << setw(8) << setprecision(5);
+		cout << "\n" << "Tong cac so duong:" << kq << endl;
+
+		FileName = getOutputFileName(i);
+		Xuat(b, k, l, FileName);
+	}
+	return 1;
+}
+
+string getInputFileName(int n)
+{
+	string fileName = string("floatmatrandata");
+	if (n < 10)
+		fileName += "0" + to_string(n) + ".inp";
+	else
+		fileName += to_string(n) + ".inp";
+	return fileName;
+}
+
+string getOutputFileName(int n)
+{
+	string fileName = string("floatmatrandata");
+	if (n < 10)
+		fileName += "0" + to_string(n) + ".out";
+	else
+		fileName += to_string(n) + ".out";
+	return fileName;
+}
+
+void Nhap(float a[][500], int& m, int& n, string filename)
+{
+	ifstream fi(filename);
+	fi >> m;
+	fi >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			fi >> a[i][j];
+}
+
+void Xuat(float a[][500], int m, int n, string filename)
+{
+	ofstream fo(filename);
+	fo << setw(8) << n;
+	fo << setw(8) << m;
+	fo << "\n";
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			fo << setw(8) << setprecision(5) << a[i][j];
+		fo << "\n";
+	}
+}
+
+void Xuat(float a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << setw(8);
+			cout << setprecision(3);
+			cout << a[i][j];
+		}
+		cout << "\n";
+	}
+}
+
+float TongDuong(float a[][500], int m, int n)
+{
+	float s = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			if (a[i][j] > 0)
+				s = s + a[i][j];
+	return s;
+}
