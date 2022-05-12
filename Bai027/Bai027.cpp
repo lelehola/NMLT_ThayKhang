04f6da146d3e0e34deb7222364ed9c9446bebb9f
anh tuan <102863743+lelehola@users.noreply.github.com>
2022-05-12 15:38:04 +0700

    Add files via upload

---
+++ Bai027/Bai027.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string getInputFileName(int);
+void Nhap(int[][500], int&, int&, string);
+void Xuat(int[][500], int, int);
+bool ktNguyenTo(int);
+int TongNguyenTo(int[][500], int, int);
+
+int main()
+{
+	int b[500][500];
+	int k;
+	int l;
+	int c;
+	for (int i = 1; i <= 1; i++)
+	{
+		string FileName = getInputFileName(i);
+		Nhap(b, k, l, FileName);
+
+		cout << "\n" << FileName << endl;
+		Xuat(b, k, l);
+
+		int S = TongNguyenTo(b, k, l);
+		cout << "\nTong cac so nguyen to: " << S;
+	}
+	return 0;
+}
+
+void Nhap(int a[][500], int& m, int& n, string filename)
+{
+	ifstream fi(filename);
+	fi >> m;
+	fi >> n;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			fi >> a[i][j];
+}
+
+string getInputFileName(int n)
+{
+	string fileName = string("intmatrandata");
+	if (n < 10)
+		fileName += "0" + to_string(n) + ".inp";
+	else
+		fileName += to_string(n) + ".inp";
+	return fileName;
+}
+
+void Xuat(int a[][500], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << setw(8) << a[i][j];
+		cout << "\n";
+	}
+}
+bool ktNguyenTo(int n)
+{
+	if (n <= 1)
+		return false;
+	for (int i = 2; i <= sqrt(n); i++)
+		if (n % i == 0)
+			return false;
+	return true;
+}
+int TongNguyenTo(int a[][500], int m, int n)
+{
+	int sum = 0;
+	for (int i = 0; i < m; i++)
+		for (int j = 1; j < n; j ++)
+			if (ktNguyenTo(a[i][j]))
+				sum += a[i][j];
+	return sum;
+}
